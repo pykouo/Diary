4c7eb5c9e35ca729c7712930844ddc68bdd8be2b
pyk <pykqqq@gmail.com>
2017-12-12 15:55:22 +0800

    register and login UI

--- app/src/main/java/com/example/pyk/multimedia/RegisterActivity.java
+++ app/src/main/java/com/example/pyk/multimedia/RegisterActivity.java
@@ -6,21 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        Button button=(Button)findViewById(R.id.btnLinkToRegisterScreen);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        Button button=(Button)findViewById(R.id.btnLinkToLoginScreen);
 
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent i = new Intent(getApplicationContext(),RegisterActivity.class);
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
             }
         });
